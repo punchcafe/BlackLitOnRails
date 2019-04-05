7e0b8b8271c311d365d7ec7d39b237209427948f
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-04-05 13:14:26 -0500

    :white_check_mark: add admin cannot see own delete button feature test

--- spec/features/admin_can_delete_admins_spec.rb
+++ spec/features/admin_can_delete_admins_spec.rb
@@ -15,6 +15,15 @@ RSpec.feature 'Admin can manage admins', type: :feature do
     expect(page).to_not have_content("scapegoat@sorry.com")
   end
 
-  scenario 'admin cannot see own delete button'
+  scenario 'admin cannot see own delete button' do
+    @admin = create(:admin)
+    auto_sign_in
+    click_on 'admins'
+    expect(page).to_not have_content("delete")
+    @admin2 = create(:admin, email: 'scapegoat@sorry.com')
+    visit('/')
+    click_on 'admins'
+    expect(page).to have_content("delete")
+  end
 
 end
