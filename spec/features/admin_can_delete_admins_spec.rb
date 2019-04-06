dbe47b18c2c0b761e9c69186b5d24090e8e93c77
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-04-06 13:56:21 -0500

    :lipstick: add admin branch styling

--- spec/features/admin_can_delete_admins_spec.rb
+++ spec/features/admin_can_delete_admins_spec.rb
@@ -19,11 +19,11 @@ RSpec.feature 'Admin can manage admins', type: :feature do
     @admin = create(:admin)
     auto_sign_in
     click_on 'admins'
-    expect(page).to_not have_content("delete")
+    expect(page).to_not have_content("X")
     @admin2 = create(:admin, email: 'scapegoat@sorry.com')
     visit('/admindashboard')
     click_on 'admins'
-    expect(page).to have_content("delete")
+    expect(page).to have_content("X")
   end
 
 end
