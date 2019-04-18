addcca3676dd2fcf76e630f02e425b9d71f90978
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-04-18 18:54:29 +0100

    :lock: anonymous user can't delete episode image

--- app/controllers/episodeimages_controller.rb
+++ app/controllers/episodeimages_controller.rb
@@ -2,12 +2,13 @@ class EpisodeimagesController < ApplicationController
   before_action :set_episode, only: [:destroy]
 
   def destroy
+    return head(401) unless current_admin
     @episode.episode_image.purge
     redirect_to admin_episodes_path
   end
 
   private
-  
+
   def set_episode
     @episode = Episode.find(params[:episode_id])
   end
