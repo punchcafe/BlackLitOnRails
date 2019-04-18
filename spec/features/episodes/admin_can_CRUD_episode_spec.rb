041ecf8717134e42eaef18c3275c06743ebe265d
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-04-18 14:57:37 +0100

    :white_check_mark: add feature test for episode image creation

--- spec/features/episodes/admin_can_CRUD_episode_spec.rb
+++ spec/features/episodes/admin_can_CRUD_episode_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../helpers/web_helpers.rb'
 
 RSpec.feature 'Admin can CRUD an episode without a picture', type: :feature do
 
-  scenario 'Admin can create episodes' do
+  scenario 'Admin can create episodes without an episode image' do
     @admin = create(:admin)
     auto_sign_in
     click_on 'episodes'
@@ -18,6 +18,27 @@ RSpec.feature 'Admin can CRUD an episode without a picture', type: :feature do
     expect(page).to have_content("the book")
     expect(page).to have_content("this was the best book ever written by anyone anywhere, ever.")
   end
+
+  scenario 'Admin can create episodes with an episode image' do
+    @admin = create(:admin)
+    auto_sign_in
+    click_on 'episodes'
+    click_on 'new'
+    fill_in "Episode Name", with: "the book"
+    fill_in 'Episode Description', with: "this was the best book ever written by anyone anywhere, ever."
+    fill_in 'Spotify Link', with: "spotify.com/lol"
+    fill_in 'Soundcloud Link', with: "soundlcoud.com/lol"
+    fill_in 'Itunes Link', with: "itunes.com/lol"
+    attach_file("Episode Image", Rails.root + "spec/fixtures/portal_logo.png")
+    click_on 'Create'
+    click_on 'episodes'
+
+    expect(page).to have_content("the book")
+    expect(page).to have_content("this was the best book ever written by anyone anywhere, ever.")
+    expect(page).to have_css("img[src*='w3schools']")
+  end
+
+
 
   scenario 'Admin can delete episodes' do
     @admin = create(:admin)
