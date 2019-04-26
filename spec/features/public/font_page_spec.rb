dfd65882478fb8aa9b6025aec20ae3c5dd79405b
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-04-26 20:12:42 +0100

    :sparkles: unlisted episodes aren't displayed

--- spec/features/public/font_page_spec.rb
+++ spec/features/public/font_page_spec.rb
@@ -6,6 +6,13 @@ RSpec.feature 'Admin can CRUD an episode', type: :feature do
   scenario 'users can see episodes on the front page' do
     @episode = create(:episode)
     visit '/'
+    expect(page.body).to have_content("the first episode")
+  end
+
+  scenario "users can't see unlisted episodes on the front page" do
+    @episode = create(:episode, episode_number: "")
+    visit '/'
+    expect(page.body).to_not have_content("the first episode")
   end
 
 end
