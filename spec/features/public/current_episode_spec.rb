a9007dca7d99e6375c6266842cb0bc6fa9b128c8
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-01 16:40:32 +0100

    :sparkles: reactive current episode title

---
+++ spec/features/public/current_episode_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require_relative '../../helpers/web_helpers.rb'
+
+RSpec.feature 'Public Next Episode Display', type: :feature do
+
+  scenario 'users can see latest episode' do
+    @episode = create(:episode)
+    visit '/'
+    expect(page.body).to have_content("Episode #01:")
+    expect(page.body).to have_content("THE FIRST EPISODE")
+  end
+
+  scenario 'users can see latest episode' do
+    @episode = create(:episode)
+    @episode = create(:episode, episode_number: 2, episode_name: "The second episode")
+    visit '/'
+    expect(page.body).to have_content("Episode #02:")
+    expect(page.body).to have_content("THE SECOND EPISODE")
+  end
+
+  scenario 'users can see latest episode, regardless of creation order' do
+    @episode = create(:episode, episode_number: 2, episode_name: "The second episode")
+    @episode = create(:episode)
+    visit '/'
+    expect(page.body).to have_content("Episode #02:")
+    expect(page.body).to have_content("THE SECOND EPISODE")
+  end
+
+end
--- spec/features/public/font_page_spec.rb
+++ spec/features/public/font_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require_relative '../../helpers/web_helpers.rb'
 
-RSpec.feature 'Admin can CRUD an episode', type: :feature do
+RSpec.feature 'Public Episode Display', type: :feature do
 
   scenario 'users can see episodes on the front page' do
     @episode = create(:episode)
