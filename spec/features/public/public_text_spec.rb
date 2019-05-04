6e64cbef8d50316a382cbea07b66f607b91785f9
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-04 16:44:02 +0100

    :sparkles: add next episode editing functionality

--- spec/features/public/public_text_spec.rb
+++ spec/features/public/public_text_spec.rb
@@ -49,24 +49,41 @@ end
 
 RSpec.feature 'Next Episode Display', type: :feature do
 
-  scenario 'Admins can edit the Mission Statement Display' do
+  scenario 'Admins can edit the Next Episode Display: name' do
     @admin = create(:admin)
     @public_text = create(:public_text, public_text_name: "Next Episode:name", body: "OLD BOOK")
-    @public_text = create(:public_text, public_text_name: "Next Episode:date", body: "2020/10/10")
+    @public_text = create(:public_text, public_text_name: "Next Episode:date", body: "2020-10-10")
     auto_sign_in
     click_on 'pages'
     expect(page).to have_content("OLD BOOK")
     expect(page).to have_content("2020-10-10")
-    click_on 'Next Episode'
-    fill_in 'Name', with: "NEW BOOK"
-    fill_in 'Date', with: "2021-11-11"
+    click_on 'Next Episode:name'
+    fill_in 'Body', with: "NEW BOOK"
     click_on 'Update'
     visit '/adminportal'
     click_on 'pages'
     expect(page).to_not have_content("OLD BOOK")
-    expect(page).to_not have_content("2020-10-10")
+    expect(page).to have_content("2020-10-10")
     expect(page).to have_content("NEW BOOK")
+  end
+
+  scenario 'Admins can edit the Next Episode Display: date' do
+    @admin = create(:admin)
+    @public_text = create(:public_text, public_text_name: "Next Episode:name", body: "OLD BOOK")
+    @public_text = create(:public_text, public_text_name: "Next Episode:date", body: "2020-10-10")
+    auto_sign_in
+    click_on 'pages'
+    expect(page).to have_content("OLD BOOK")
+    expect(page).to have_content("2020-10-10")
+    click_on 'Next Episode:date'
+    fill_in 'Body', with: "2021-11-11"
+    click_on 'Update'
+    visit '/adminportal'
+    click_on 'pages'
+    expect(page).to have_content("OLD BOOK")
+    expect(page).to_not have_content("2020-10-10")
     expect(page).to have_content("2021-11-11")
   end
+
 
 end
