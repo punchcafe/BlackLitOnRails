7886839e392ec3f5c4c9a62542a6d2c7f7ca27fc
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-07 18:49:06 +0100

    :card_file_box: add ENV variable adaptability to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,33 @@ PublicText.create( public_text_name: "Mission Statement", body: "") if PublicTex
 PublicText.create( public_text_name: "Next Episode:name", body: "") if PublicText.where(public_text_name: "Next Episode:name").length == 0
 PublicText.create( public_text_name: "Next Episode:date", body: "") if PublicText.where(public_text_name: "Next Episode:date").length == 0
 
+def match_original_salt?(admin, current_salt)
+  #checks if salt ENV variable has been changed
+  current_encrypted_password = BCrypt::Password.new(admin.encrypted_password)
+  expected_salted_password = current_salt+"helloworld"
+  current_encrypted_password == expected_salted_password
+end
 
-if Admin.where(email: "alpha").length == 0
-  salt = ENV['hashsalt'] ? ENV['hashsalt'] : ""
+admin_search = Admin.where(email: "alpha")
+salt = ENV['hashsalt'] ? ENV['hashsalt'] : ""
+
+
+if admin_search.length == 0
+  #Do not change helloworld password base
   encrypted_password = BCrypt::Password.create(salt+"helloworld")
-
   Admin.create(
     email: "alpha",
     encrypted_password: encrypted_password
   )
+elsif admin_search.length == 1
+  #if salt ENV has been changed, updates alpha password to "helloworld with new salt"
+  unless match_original_salt?(admin_search[0], salt)
+    admin_search[0].update(
+      encrypted_password: BCrypt::Password.create(salt+"helloworld")
+    )
+  end
 end
+
+
 
 # change for environment
