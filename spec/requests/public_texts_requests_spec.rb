04d0381dd52216f2d5d0fbcb75fdbabf789bb7af
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-07 13:20:41 +0100

    :white_check_mark: added controller test for admins

--- spec/requests/public_texts_requests_spec.rb
+++ spec/requests/public_texts_requests_spec.rb
@@ -7,5 +7,19 @@ describe "Public Texts", type: :request do
       put "/admin/public_texts/#{@public_text.id}", :params => {:public_text => { :body => "Hacked it!" }}
       expect(response).to have_http_status 401
     end
+
+    it "admin may edit public text" do
+      auto_sign_in
+      @public_text = create(:public_text)
+      put "/admin/public_texts/#{@public_text.id}", :params => {:public_text => { :body => "Hacked it!" }}
+      expect(response).to_not have_http_status 401
+      expect(PublicText.find(@public_text.id).body).to eq("Hacked it!")
+    end
+  end
+
+
+  def auto_sign_in
+    @admin = create(:admin)
+    post "/session", :params => {email: "testboy@aol.com", password: "helloworld" }
   end
 end
