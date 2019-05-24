78f8b5f3cc984d2c077bf8696fdacd4a99575397
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-05-24 15:37:39 +0100

    :bug: fix dissapearing nav bar

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,13 +1,34 @@
+window.addEventListener('hashchange', function(e){
+  console.log("hashchange")
+  document.getElementById("public_nav").style.display = "block"
+}, false);
+
+window.onhashchange = function () {
+  console.log("hashchange")
+}
+
+
+
   window.addEventListener('scroll', function(e) {
-    if(window.scrollY<210 && window.scrollY>25){
+    if(window.scrollY<195 && window.scrollY>25){
       document.getElementById("logo_centerer").style.height = (250-window.scrollY+25).toString() +"px"
     }else{
       document.getElementById("logo_centerer").style.height = "50 px"
     }
     if(window.scrollY<275){
-      document.getElementById("public_nav").style.display = "none"
+      hideNav()
     }else{
-      document.getElementById("public_nav").style.display = "block"
+      showNav()
     }
-    console.log(window.scrollY);
   });
+
+window.onload = hideNav
+
+function hideNav(){
+  document.getElementById("public_nav").style.display = "none"
+}
+
+function showNav(){
+  console.log("showing!")
+  document.getElementById("public_nav").style.display = "block"
+}
